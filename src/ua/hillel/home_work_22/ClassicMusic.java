2002a6d4885ad2b7f5b3c4bc8cc517e86a486d2b
ArtemZhuravlev <Utah954@gmail.com>
2023-11-12 02:32:47 +0200

    Change in code

--- src/ua/hillel/home_work_22/ClassicMusic.java
+++ src/ua/hillel/home_work_22/ClassicMusic.java
@@ -7,7 +7,6 @@ public class ClassicMusic extends MusicStyles {
         System.out.println("\nClassic bands and artists: Johann Sebastian Bach, " +
                 "Ludwig van Beethoven, Vivaldi.");
     }
-
     @Override
     public void playMusic() {
         System.out.println("These bands and artists play Classical music.");
--- src/ua/hillel/home_work_22/PopMusic.java
+++ src/ua/hillel/home_work_22/PopMusic.java
@@ -7,7 +7,6 @@ public class PopMusic extends MusicStyles {
         System.out.println("\nPop bands and artists: Coldplay, Simple plan, Avril Lavigne, " +
                 "Imagine Dragons.");
     }
-
     @Override
     public void playMusic() {
         System.out.println("These bands and artists play Pop music.");
