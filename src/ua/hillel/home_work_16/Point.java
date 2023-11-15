8619b6de6b3bba1643a2835c8dab574322f6db0c
ArtemZhuravlev <Utah954@gmail.com>
2023-11-15 03:21:32 +0200

    *

--- src/ua/hillel/home_work_16/Point.java
+++ src/ua/hillel/home_work_16/Point.java
@@ -4,6 +4,7 @@ import java.util.Objects;
 
 public class Point implements Cloneable {
 
+
     private int x;
     private int y;
 
@@ -21,8 +22,11 @@ public class Point implements Cloneable {
         this.y = point.y;
     }
 
+
+    public int getX() {return x;}
     public void setX(int x) {this.x = x;}
 
+    public int getY() {return y;}
     public void setY(int y) {this.y = y;}
 
 
